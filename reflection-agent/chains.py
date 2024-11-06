95605ee3d39e70f5147cf442cfe35a29b88cb7aa
echeadle <echeadle@gmail.com>
2024-11-06 09:34:35 -0700

    Finished reflection agent

--- reflection-agent/chains.py
+++ reflection-agent/chains.py
@@ -1,28 +1,19 @@
-from langchain_core.prompts.chat import ChatMessagePromptTemplate, MessagesPlaceholder
+from langchain_core.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 
-reflection_prompt = ChatMessagePromptTemplate(
-    [
-        {
-            "system",
-            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
-            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
-        },
-        MessagesPlaceholder(variable_name="messages")
-    ]
-)
+reflection_prompt = ChatPromptTemplate([
+        ("system", """You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet.
+         Always provide detailed recommendations, including requests for length, virality, style, etc."""),
+         MessagesPlaceholder(variable_name="messages", optional=True)
+])
 
-generation_prompt = ChatMessagePromptTemplate(
-    [
-        {
-            "system",
-            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
-            " Generate the best twitter post possible for the user's request."
-            " If the user provides critique, respond with a revised version of your previous attempts.",
-        },
-        MessagesPlaceholder(variable_name="messages")
-    ]
-)
+generation_prompt = ChatPromptTemplate([
+        ("system",
+        """You are a twitter techie influencer assistant tasked with writing excellent twitter posts.
+            Generate the best twitter post possible for the user's request.
+            If the user provides critique, respond with a revised version of your previous attempts."""),
+        MessagesPlaceholder(variable_name="messages", optional=True)
+])
 
 llm = ChatOpenAI(model= "gpt-4o-mini")
 generate_chain = generation_prompt | llm
