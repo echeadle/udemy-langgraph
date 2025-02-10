e9866a87a7bda431e66b52d3fb217b7d6aa15d7d
echeadle <echeadle@gmail.com>
2025-02-10 09:01:46 -0700

    Add reflexion-agent

---
+++ reflexion-agent/chains.py
@@ -0,0 +1,36 @@
+import datetime
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
+
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+actor_prompt_template = ChatPromptTemplate([
+    (
+        "system",
+    """
+        You are an exper researcher.
+    Current time: {time}
+    
+1. {first_instruction}
+2. Reflect and critique your answer. Be severe to maximize improvement.
+3. Recomend search queries to research information and improve your answers.
+""",
+    ),
+    MessagesPlaceholder(variable_name="messages"),
+    ("system", "Answer the user's question above using the required format.")
+    ]).partial(
+        time=lambda: datetime.datetime.now().isoformat(),
+    )
---
+++ reflexion-agent/schemas.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing")
+    superfluous: str = Field(description="Critique of what is superfluous")
+    
+class AnswerQuestion(BaseModel):
+    """Answer the Question"""
+    
+    answer: str = Field(description="~250 word detailed answer to the question")
+    reflection: Reflection = Field(description="Your refelction on the initial answer.")
+    search_queries: List[str] = Field(
+        description="1-3 search queries for researching imporovements to address the critique of your current answer."
+    )
